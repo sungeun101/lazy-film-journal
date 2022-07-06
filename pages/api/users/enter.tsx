e19e97d5aaae32aada8b82af12aef56786620fab
sungeun101 <josungeun1019@gmail.com>
2022-07-06 22:09:24 +0900

    feat: add Token model and ConnectOrCreate user with token

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -4,18 +4,25 @@ import withHandler from "@libs/server/withHandler";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { email, phone } = req.body;
-  const payload = email ? { email } : { phone: +phone };
-  const user = await client.user.upsert({
-    where: {
-      ...payload,
-    },
-    create: {
-      name: "Anonymous",
-      ...payload,
-    },
-    update: {},
-  });
-  console.log(user);
+  const userInput = email ? { email } : { phone: +phone };
+const payload = Math.floor(100000+Math.random()*900000)+'';
+  const token=await client.token.create({
+    data:{
+        payload,
+        user:{
+            connectOrCreate:{
+                where:{
+                    ...userInput 
+                },
+                create:{
+                    name:"Anonymous",
+                    ...userInput 
+                }
+            }
+        }
+    }
+  })
+  console.log(token);
   return res.status(200).end();
 }
 

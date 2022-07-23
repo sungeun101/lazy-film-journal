46909afd0533e2eee3b0cd669b7add1c11779945
sungeun101 <josungeun1019@gmail.com>
2022-07-23 22:05:28 +0900

    feat: get product detail with user info

---
+++ pages/api/products/[id].ts
@@ -0,0 +1,36 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const { id } = req.query;
+  const product = await client.product.findUnique({
+    where: {
+      id: Number(id),
+    },
+    include: {
+      user: {
+        select: {
+          id: true,
+          name: true,
+          avatar: true,
+        },
+      },
+    },
+  });
+  res.json({
+    ok: true,
+    product,
+  });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ["GET"],
+    handler,
+  })
+);
--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,8 +1,15 @@
 import Button from "@components/button";
 import Layout from "@components/layout";
 import type { NextPage } from "next";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import useSWR from "swr";
 
-const ItemDetail: NextPage = () => {
+const ProductDetail: NextPage = () => {
+  const router = useRouter();
+  const { data } = useSWR(
+    router.query ? `/api/products/${router.query.id}` : null
+  );
   return (
     <Layout canGoBack>
       <div className="px-4 py-10">
@@ -11,24 +18,25 @@ const ItemDetail: NextPage = () => {
           <div className="flex cursor-pointer py-3 border-t border-b items-center space-x-3">
             <div className="w-12 h-12 rounded-full bg-slate-300" />
             <div>
-              <p className="text-sm font-medium text-gray-700">Steve Jebs</p>
-              <p className="text-xs font-medium text-gray-500">
-                View profile &rarr;
+              <p className="text-sm font-medium text-gray-700">
+                {data ? data.product.user.name : "..."}
               </p>
+              <Link href={`/users/profiles/${data?.product?.user?.id}`}>
+                <a className="text-xs font-medium text-gray-500">
+                  View profile &rarr;
+                </a>
+              </Link>
             </div>
           </div>
           <div className="mt-5">
-            <h1 className="text-3xl font-bold text-gray-900">Galaxy S50</h1>
-            <span className="text-2xl block mt-3 text-gray-900">$140</span>
+            <h1 className="text-3xl font-bold text-gray-900">
+              {data ? data.product.name : "..."}
+            </h1>
+            <span className="text-2xl block mt-3 text-gray-900">
+              {data ? `$${data.product.price}` : "..."}
+            </span>
             <p className=" my-6 text-gray-700">
-              My money&apos;s in that office, right? If she start giving me some
-              bullshit about it ain&apos;t there, and we got to go someplace
-              else and get it, I&apos;m gonna shoot you in the head then and
-              there. Then I&apos;m gonna shoot that bitch in the kneecaps, find
-              out where my goddamn money is. She gonna tell me too. Hey, look at
-              me when I&apos;m talking to you, motherfucker. You listen: we go
-              in there, and that ni**a Winston or anybody else is in there, you
-              the first motherfucker to get shot. You understand?
+              {data ? data.product.description : "..."}
             </p>
             <div className="flex items-center justify-between space-x-2">
               <Button large text="Talk to seller" />
@@ -69,4 +77,4 @@ const ItemDetail: NextPage = () => {
   );
 };
 
-export default ItemDetail;
+export default ProductDetail;

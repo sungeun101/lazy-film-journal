3e876ea4554d28f87eeaf04c49b717cd32d62b44
sungeun101 <josungeun1019@gmail.com>
2022-07-26 02:01:52 +0900

    feat: (api)like/unlinke products

---
+++ pages/api/products/[id]/fav.ts
@@ -0,0 +1,51 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    query: { id },
+    // id in the URL
+    session: { user },
+  } = req;
+  const alreadyExists = await client.fav.findFirst({
+    where: {
+      productId: Number(id),
+      userId: user?.id,
+    },
+  });
+  if (alreadyExists) {
+    await client.fav.delete({
+      where: {
+        id: alreadyExists.id,
+      },
+    });
+  } else {
+    await client.fav.create({
+      data: {
+        user: {
+          connect: {
+            id: user?.id,
+          },
+        },
+        product: {
+          connect: {
+            id: Number(id),
+          },
+        },
+      },
+    });
+  }
+  res.json({ ok: true });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ["POST"],
+    handler,
+  })
+);
